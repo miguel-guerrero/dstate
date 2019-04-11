ad52974584bfc6a94b022028c6f38632d1aa47e2
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-04-11 08:13:21 -0700

    Updated

--- VlogParser.py
+++ VlogParser.py
@@ -1,7 +1,25 @@
 #------------------------------------------------------------------------------
-# VLOG simplified parsing
+# Copyright (c) 2018-Present, Miguel A. Guerrero
+# All rights reserved.
+# 
+# This is free software released under GNU Lesser GPL license version 3.0 (LGPL 3.0)
+# 
+# See http://www.gnu.org/licenses/lgpl-3.0.txt for a full text
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# Please send bugs and suggestions to: miguel.a.guerrero@gmail.com
 #------------------------------------------------------------------------------
-# Author: Miguel A. Guerrero
+# VLOG simplified parsing
 #------------------------------------------------------------------------------
 from TopDown import TopDown, TokenAttrs
 from enum import Enum
