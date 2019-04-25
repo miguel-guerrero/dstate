767cfa9af78942e27451e4b6e33ab2553777773d
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-04-24 20:22:29 -0700

    improved

--- install_iverilog.sh
+++ install_iverilog.sh
@@ -2,6 +2,16 @@
 
 inst_dir=$HOME/iverilog
 
+export TARGET_ARCH=  #in mac-os if this is set, linking fails
+
+error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+#check if wget is available, if not exit
+which wget || error_exit "install wget before proceeding please. Exiting.."
+
 #use this script to install from source in a local directory (no need for root
 #access). If the install directory is changed from the default above, make sure 
 #to update common/include.mk PATH variable accordingly or have the new path/bin
@@ -15,3 +25,9 @@ cd verilog-$ver
 cd ..
 rm -rf verilog-$ver
 rm -f verilog-$ver.tar.gz
+
+echo -e "Remember to add the following to your .profile or equivalent:\n"
+echo -e "    export PATH=$inst_dir/bin:\$PATH"
+echo -e "\nor\n"
+echo -e "    set path=($inst_dir/bin \$path)"
+echo -e "\ndepending on your shell"
